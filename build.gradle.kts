a4bd81081182b36df8148aaadd63140131e5b498
go-tiger <dev.gotiger@gmail.com>
2026-07-04 06:53:29 +0900

    chore: GTDonationCore 의존성 및 chzzk4j 연동을 위한 빌드 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,28 @@
 plugins {
     id("java-library")
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("dev.gotiger:GTDonationCore:0.1.0")
+    implementation("io.github.r2turntrue:chzzk4j:0.1.4")
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 tasks {
